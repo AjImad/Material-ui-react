3c2af44e2a7367a71f5457cf5932342ec36163de
Ajbar Imad <ajbar.imad.99@gmail.com>
2022-04-04 00:03:26 +0000

    adding Add page for form

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Fab, TextField, Tooltip, Typography } from '@mui/material';
+import { Button, Fab, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Snackbar, TextField, Tooltip, Typography } from '@mui/material';
 import Modal from '@mui/material/Modal';
 import AddIcon from '@mui/icons-material/Add';
 import { Box, createTheme } from '@mui/system';
+import MuiAlert from '@mui/material/Alert';
 
 export default function Feed() {
     
     const [open, setOpen] = useState(false)
-    const handleClose = () => setOpen(false)
+    
+    const handleClose = () => setOpen(false);
+
+    const [openAlert, setOpenAlert] = useState(false)
+
+    const handleCloseAlert = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpenAlert(false);
+      };
+
+    const [visible, setVisible] = React.useState('');
+
+    const handleChange = (event) => {
+        setVisible(event.target.value);
+    };
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
 
     const theme = createTheme({
         breakpoints:{
@@ -28,14 +50,15 @@ export default function Feed() {
         left: '50%',
         transform: 'translate(-50%, -50%)',
         width: 500,
-        height: 500,
+        height: 550,
         bgcolor: 'background.paper',
         backgroundColor: '#fff',
-        p: 4,
+        p: 3,
+        borderRadius: 3,
         [theme.breakpoints.down('sm')]:{
             width: '100vw',
             height: '100vh',
-        }
+        },
       };
 
   return (
@@ -59,20 +82,58 @@ export default function Feed() {
             sx={style} 
             component="form"
             noValidate
-            autoComplete='off'    
-        >
-            <TextField color="warning" id="outlined-basic" label="Title" variant="standard" sx={{width: '100%'}} />
+            autoComplete='off'   
+             
+         >
+            <TextField id="outlined-basic" label="Title" variant="standard" sx={{width: '100%', mb: 3}} />
             <TextField
                 id="outlined-multiline-static"
                 label="Description"
                 multiline
                 rows={4}
                 defaultValue="Tell Your Story..."
-                color="warning"
-                sx={{width: '100%'}}
+                sx={{width: '100%', mb: 3}}
             />
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, mb: 3}}>
+                <InputLabel id="demo-simple-select-filled-label">Visibility</InputLabel>
+                <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                value={visible}
+                onChange={handleChange}
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value="Public">Public</MenuItem>
+                    <MenuItem value="Private">Private</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{display: 'block', mb: 3}}>
+                <FormLabel id="demo-radio-buttons-group-label">Who can comment?</FormLabel>
+                <RadioGroup>
+                    <FormControlLabel value="Everybody" control={<Radio />} label="Everybody" />
+                    <FormControlLabel value="My Friends" control={<Radio />} label="My Friends" />
+                    <FormControlLabel value="Nobody" control={<Radio />} label="Nobody" />
+                    <FormControlLabel value="Custom" disabled control={<Radio />} label="Custom (Premuim)" />
+                </RadioGroup>
+            </FormControl>
+            <Button 
+                variant="outlined" color="success" 
+                sx={{mr: 3}}
+                onClick={() => {setOpenAlert(true)}}
+                >Create</Button>
+            <Button 
+                variant="outlined" color="error"
+                onClick={() => {handleClose()}}
+            >Cancel</Button>
         </Box>
-    </Modal>
+     </Modal>
+     <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleCloseAlert} severity="success" sx={{ width: '100%' }}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </>
   );
 }
